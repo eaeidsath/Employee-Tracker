8bf520075b54fa6161b38a7490aa66543f77b06e
eaeidsath <evelyneidsath@gmail.com>
2024-02-05 01:35:03 -0800

    define tables in schema.sql, create query.sql, minor adjustments to server and json

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
